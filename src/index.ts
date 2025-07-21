a6d8892ec298aa7ac87f8777971e24ddeb16eaf4
gdar463 <gdar463@gmail.com>
2025-07-21 21:16:15 +0200

    added starting discord bot

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import chalk from "chalk";
+import { Client, Events, GatewayIntentBits } from "discord.js";
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (client) => {
+  console.log(
+    `${chalk.greenBright("READY")} Logged in as ${chalk.cyanBright(client.user.username)}`,
+  );
+});
+client.login(process.env.DISCORD_TOKEN);
+
