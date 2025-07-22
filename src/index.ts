f0b012fc48a0a3da8c73f3aea24108da76efc7e9
gdar463 <gdar463@gmail.com>
2025-07-22 15:06:23 +0200

    added logging commands

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,11 @@ client.on(Events.InteractionCreate, (interaction) => {
   if (!interaction.isChatInputCommand()) return;
   const { commandName } = interaction;
   if (commands[commandName]) {
+    logger.verbose(
+      `[${chalk.magentaBright(interaction.guild?.name)} (${chalk.blackBright(interaction.guildId)})] ` +
+        `${chalk.blueBright(interaction.user.username)} (${chalk.blackBright(interaction.user.id)}): ` +
+        `${chalk.whiteBright(commandName)}`,
+    );
     commands[commandName].execute(interaction);
   }
 });
