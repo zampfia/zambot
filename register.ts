398e1d7793acab38137ab04698f4d5c3fd606f77
gdar463 <gdar463@gmail.com>
2025-07-22 15:53:36 +0200

    added option to register commands globally

--- register.ts
+++ register.ts
@@ -7,15 +7,42 @@ const commandsData = Object.entries(commands).map((val) => {
   return val[1].data.toJSON();
 });
 
-console.log(`Refreshing ${Object.keys(commands).length} slash commands...`);
+if (process.argv[2] === "global") {
+  await refreshGlobal();
+} else {
+  await refreshGuild();
+}
 
-const data = await rest.put(
-  Routes.applicationGuildCommands(
-    process.env.DISCORD_CLIENT_ID!,
-    process.env.DISCORD_TEST_GUILD_ID!,
-  ),
-  { body: commandsData },
-);
+async function refreshGlobal() {
+  console.log(
+    `Refreshing ${Object.keys(commands).length} slash commands GLOBALLY...`,
+  );
+  console.log("Stop it in the next 5 seconds if by error");
 
-// @ts-ignore data is a list, but discordjs doesn't type it
-console.log(`Refreshed ${data.length} slash commands\nDONE`);
+  setTimeout(async () => {
+    const data = await rest.put(
+      Routes.applicationCommands(process.env.DISCORD_CLIENT_ID!),
+      { body: commandsData },
+    );
+
+    // @ts-ignore data is a list, but discordjs doesn't type it
+    console.log(`Refreshed ${data.length} slash commands GLOBALLY\nDONE`);
+  }, 5_000);
+}
+
+async function refreshGuild() {
+  console.log(
+    `Refreshing ${Object.keys(commands).length} slash commands for guild id ${process.env.DISCORD_TEST_GUILD_ID}...`,
+  );
+
+  const data = await rest.put(
+    Routes.applicationGuildCommands(
+      process.env.DISCORD_CLIENT_ID!,
+      process.env.DISCORD_TEST_GUILD_ID!,
+    ),
+    { body: commandsData },
+  );
+
+  // @ts-ignore data is a list, but discordjs doesn't type it
+  console.log(`Refreshed ${data.length} slash commands for guild\nDONE`);
+}
